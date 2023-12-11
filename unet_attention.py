5c0b750968a95c292226dc4f38af13f75d313aa1
xavierdurawa <xavierdurawa@gmail.com>
2023-12-10 21:44:05 -0500

    Update unet_attention.py

--- unet_attention.py
+++ unet_attention.py
@@ -95,8 +95,8 @@ class UNetAttn(nn.Module):
             
         self.attention_gates = nn.ModuleList()
         for i in range(len(channel_list)):
-            # self.attention_gates.append(AttentionGate(channel_list[i], channel_list[i], channel_list[i]//2))
-            self.attention_gates.append(AttentionGate(channel_list[i], channel_list[i], channel_list[i]))
+            self.attention_gates.append(AttentionGate(channel_list[i], channel_list[i], channel_list[i]//2))
+            # self.attention_gates.append(AttentionGate(channel_list[i], channel_list[i], channel_list[i]))
 
         self.bottleneck = DoubleConv(curr_channel, curr_channel * 2, curr_channel)
 
